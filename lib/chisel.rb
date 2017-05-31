5246abe3ffe62d2bc5362b1dff2cb68b3a0b7c13
JF-Lalonde <lalondej88@gmail.com>
2017-05-31 13:37:21 -0600

    first test passes

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,5 +1,12 @@
 class Chisel
+  attr_reader :markdown_text
+
   def initialize(input_file)
-    File.open(input_file)
+    @markdown_text = File.read(input_file)
+  end
+
+  def break_into_lines
+    markdown_text
+
   end
 end
--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -3,5 +3,10 @@ require 'minitest/pride'
 require './lib/chisel'
 
 class ChiselTest < Minitest::Test
-  
+  def test_it_can_open_file
+    chisel = Chisel.new("./lib/something.markdown")
+    expected = "#Some Words\n"
+
+    assert_equal expected, chisel.markdown_text
+  end
 end
